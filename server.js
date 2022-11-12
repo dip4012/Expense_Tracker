acc7c85212eb9a60d4102777d281907e364dd440
aritra <chatterjee.aritra@protonmail.com>
2022-11-12 15:22:20 +0530

    port fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 
 require('dotenv').config({path:"./config.env"});
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 //middlewares
 app.use(cors());
